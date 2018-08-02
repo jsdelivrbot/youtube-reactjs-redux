49b7ac1e19e669ceb0ed889693a8d573e02e67af
thanh nguyen <thanhnguyen@thanhs-MacBook-Pro.local>
2018-08-03 06:45:56 +0700

    update

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,7 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     const video_items = props.videos.map((video) => {
-        return <VideoListItem video={video}/>
+        return <VideoListItem key={video.etag} video={video}/>
     });
     return (
         <ul className="col-md-4 list-group">
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const VideoListItem = () => {
+const VideoListItem = ({video}) => {
+    const imageURL = video.snippet.thumbnails.default.url;
+    const videoTitle = video.snippet.title;
     return (
-        <li> Video</li>
+        <li className="list-group-item"> 
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageURL}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{videoTitle}</div>
+                </div>
+            </div>
+        </li>
     );
 }
 
